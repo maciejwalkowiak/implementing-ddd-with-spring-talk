13ed9f9a7a41f0f246d947e1294cbf164ee212d3
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2024-06-04 12:50:07 +0200

    book search service in infrastructure module

--- src/main/java/library/catalog/application/AddBookToCatalogUseCase.java
+++ src/main/java/library/catalog/application/AddBookToCatalogUseCase.java
@@ -1,12 +1,25 @@
 package library.catalog.application;
 
+import library.catalog.domain.Book;
+import library.catalog.domain.BookRepository;
 import library.catalog.domain.Isbn;
+import library.catalog.infrastructure.BookSearchService;
+import library.catalog.infrastructure.OpenLibraryIsbnSearchResult;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddBookToCatalogUseCase {
+    private final BookSearchService bookSearchService;
+    private final BookRepository bookRepository;
+
+    public AddBookToCatalogUseCase(BookSearchService bookSearchService, BookRepository bookRepository) {
+        this.bookSearchService = bookSearchService;
+        this.bookRepository = bookRepository;
+    }
 
     public void execute(Isbn isbn) {
-
+        OpenLibraryIsbnSearchResult result = bookSearchService.search(isbn);
+        Book book = new Book(result.title(), isbn);
+        bookRepository.save(book);
     }
 }
