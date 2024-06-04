3dc0a727cf50d41d4df1a8e42093e93aabe1a00c
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2024-06-04 12:50:07 +0200

    initial commit

---
+++ src/test/java/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
